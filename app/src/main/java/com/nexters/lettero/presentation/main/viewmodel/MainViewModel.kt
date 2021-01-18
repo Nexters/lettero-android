51380c2dd24c52226bd579831cf4d66c58de7765
dlwlrma <dlwlrma.io@gmail.com>
2021-01-19 01:51:19 +0900

    dagger hilt

--- app/src/main/java/com/nexters/lettero/presentation/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/nexters/lettero/presentation/main/viewmodel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.nexters.lettero.presentation.main.viewmodel
 
 import com.nexters.lettero.presentation.base.ViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel {}
+class MainViewModel @Inject constructor() : ViewModel {}
