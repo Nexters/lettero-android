2a1112b7e524e516061de162634e6a35ad8921e3
dlwlrma <dlwlrma.io@gmail.com>
2021-01-20 01:51:18 +0900

    google firebase cloud messaging

--- app/src/main/java/com/nexters/lettero/MainActivity.kt
+++ app/src/main/java/com/nexters/lettero/MainActivity.kt
@@ -2,10 +2,32 @@ package com.nexters.lettero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(
+                    MainActivity::class.qualifiedName,
+                    "Fetching FCM registration token failed",
+                    task.exception
+                )
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+            token?.let { // Log and toast
+                Log.d(MainActivity::class.qualifiedName, token)
+                Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/nexters/lettero/MyFirebaseMessagingService.kt
@@ -0,0 +1,53 @@
+package com.nexters.lettero
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+        Log.i("FirebaseCloudMessaging", token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        notification(remoteMessage)
+    }
+
+    private fun notification(remoteMessage: RemoteMessage) {
+        val channelId = packageName
+        val notificationId = 0
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "lettero"
+            val channelDescription = "alarm notification channel for lettero"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        var builder = NotificationCompat.Builder(this@MyFirebaseMessagingService, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(remoteMessage.notification?.title)
+            .setContentText(remoteMessage.notification?.body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this@MyFirebaseMessagingService)) {
+            notify(notificationId, builder.build())
+        }
+    }
+}
