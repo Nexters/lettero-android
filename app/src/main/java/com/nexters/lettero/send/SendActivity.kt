7777e2c99f389f967dd03b6a3d45a989fd1f44f6
songmilee <lsm941010@gmail.com>
2021-01-30 16:31:06 +0900

    메세지 작성화면 레이아웃 작성

---
+++ app/src/main/java/com/nexters/lettero/send/SendActivity.kt
@@ -0,0 +1,12 @@
+package com.nexters.lettero.send
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nexters.lettero.R
+
+class SendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send);
+    }
+}
