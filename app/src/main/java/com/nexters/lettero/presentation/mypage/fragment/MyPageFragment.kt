d78634bf216c51d4623aec19d6b0bb88cc16b875
songmilee <lsm941010@gmail.com>
2021-02-08 23:52:11 +0900

    마이 페이지 뷰모델과 뷰 연결

--- app/src/main/java/com/nexters/lettero/presentation/mypage/fragment/MyPageFragment.kt
+++ app/src/main/java/com/nexters/lettero/presentation/mypage/fragment/MyPageFragment.kt
@@ -1,5 +1,10 @@
 package com.nexters.lettero.presentation.mypage.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.nexters.lettero.R
 import com.nexters.lettero.databinding.FragmentMyPageBinding
 import com.nexters.lettero.presentation.base.BaseFragment
@@ -9,4 +14,15 @@ import com.nexters.lettero.presentation.mypage.viewmodel.MyPageViewModel
 class MyPageFragment : BaseFragment<FragmentMyPageBinding, MyPageViewModel>() {
     override val layoutRes: Int = R.layout.fragment_my_page
     override var viewModel: ViewModel = MyPageViewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentMyPageBinding>(layoutInflater, layoutRes, container, false)
+        binding.vm = viewModel as MyPageViewModel
+
+        return binding.root
+    }
 }
--- app/src/main/java/com/nexters/lettero/presentation/mypage/viewmodel/MyPageViewModel.kt
+++ app/src/main/java/com/nexters/lettero/presentation/mypage/viewmodel/MyPageViewModel.kt
@@ -1,5 +1,18 @@
 package com.nexters.lettero.presentation.mypage.viewmodel
 
+import androidx.compose.ui.res.stringResource
+import androidx.databinding.ObservableField
+import com.nexters.lettero.R
 import com.nexters.lettero.presentation.base.ViewModel
 
-class MyPageViewModel : ViewModel {}
+class MyPageViewModel : ViewModel {
+
+    val name = ObservableField<String>()
+    val phoneNumber = ObservableField<String>()
+
+    init {
+        //TODO : 사용자 정보 연동 후 변경 예정
+        name.set(stringResource(R.string.app_name))
+        phoneNumber.set("010-0000-0000")
+    }
+}
