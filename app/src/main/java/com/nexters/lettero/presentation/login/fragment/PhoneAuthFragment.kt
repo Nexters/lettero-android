3018fe7b7d4ccb24bdf9715f97a47c774516b279
songmilee <lsm941010@gmail.com>
2021-02-10 23:25:53 +0900

    휴대폰 번호 인증 화면 작성

---
+++ app/src/main/java/com/nexters/lettero/presentation/login/fragment/PhoneAuthFragment.kt
@@ -0,0 +1,12 @@
+package com.nexters.lettero.presentation.login.fragment
+
+import com.nexters.lettero.R
+import com.nexters.lettero.databinding.FragmentLoginBinding
+import com.nexters.lettero.presentation.base.BaseFragment
+import com.nexters.lettero.presentation.base.ViewModel
+import com.nexters.lettero.presentation.login.viewmodel.PhoneAuthViewModel
+
+class PhonAuthFragment : BaseFragment<FragmentLoginBinding, PhoneAuthViewModel>() {
+    override val layoutRes: Int = R.layout.fragment_phone_auth
+    override var viewModel: ViewModel = PhoneAuthViewModel()
+}
---
+++ app/src/main/java/com/nexters/lettero/presentation/login/viewmodel/PhoneAuthViewModel.kt
@@ -0,0 +1,5 @@
+package com.nexters.lettero.presentation.login.viewmodel
+
+import com.nexters.lettero.presentation.base.ViewModel
+
+class PhoneAuthViewModel : ViewModel {}
