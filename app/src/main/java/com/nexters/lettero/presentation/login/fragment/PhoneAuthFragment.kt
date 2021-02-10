82c8e2d501e7dae65db67b44abda2793b47dde47
songmilee <lsm941010@gmail.com>
2021-02-10 23:46:07 +0900

    일반적인 UI 작업 처리

--- app/src/main/java/com/nexters/lettero/presentation/login/fragment/PhoneAuthFragment.kt
+++ app/src/main/java/com/nexters/lettero/presentation/login/fragment/PhoneAuthFragment.kt
@@ -9,4 +9,6 @@ import com.nexters.lettero.presentation.login.viewmodel.PhoneAuthViewModel
 class PhonAuthFragment : BaseFragment<FragmentLoginBinding, PhoneAuthViewModel>() {
     override val layoutRes: Int = R.layout.fragment_phone_auth
     override var viewModel: ViewModel = PhoneAuthViewModel()
+
+    
 }
--- app/src/main/java/com/nexters/lettero/presentation/login/viewmodel/PhoneAuthViewModel.kt
+++ app/src/main/java/com/nexters/lettero/presentation/login/viewmodel/PhoneAuthViewModel.kt
@@ -1,5 +1,39 @@
 package com.nexters.lettero.presentation.login.viewmodel
 
+import android.os.CountDownTimer
+import androidx.databinding.ObservableField
 import com.nexters.lettero.presentation.base.ViewModel
 
-class PhoneAuthViewModel : ViewModel {}
+class PhoneAuthViewModel : ViewModel {
+    val phoneNumber = ObservableField<String>()
+    val confrimNumber = ObservableField<String>()
+    val authTimer = ObservableField<String>()
+    val SEC_INTERVAL = 1000L
+    val MAX_SECOND = SEC_INTERVAL * 60 * 3 // 3분
+
+    val countDownTimer = object : CountDownTimer(MAX_SECOND, SEC_INTERVAL) {
+        override fun onFinish() {
+            TODO("Not yet implemented")
+        }
+
+        override fun onTick(millisUntilFinished: Long) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    init {
+        authTimer.set("")
+        confrimNumber.set("")
+    }
+
+    // TODO : 휴대폰 번호 인증 요청
+    fun requestPhoneNumberAuth() {
+
+    }
+
+    // TODO : 휴대폰 인증 완료
+    fun confirmPhoneNumber() {
+
+    }
+}
