a66f44bba1d970156f7876ab20c4b3bce85b70b4
songmilee <lsm941010@gmail.com>
2021-02-12 12:11:58 +0900

    사용자 전화번호 추출

--- app/src/main/java/com/nexters/lettero/presentation/login/viewmodel/PhoneAuthViewModel.kt
+++ app/src/main/java/com/nexters/lettero/presentation/login/viewmodel/PhoneAuthViewModel.kt
@@ -2,38 +2,51 @@ package com.nexters.lettero.presentation.login.viewmodel
 
 import android.os.CountDownTimer
 import androidx.databinding.ObservableField
+import androidx.navigation.NavController
+import com.nexters.lettero.R
 import com.nexters.lettero.presentation.base.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PhoneAuthViewModel : ViewModel {
     val phoneNumber = ObservableField<String>()
-    val confrimNumber = ObservableField<String>()
+    val rtnNumber = ObservableField<String>()
     val authTimer = ObservableField<String>()
+    lateinit var navController: NavController
+
     val SEC_INTERVAL = 1000L
     val MAX_SECOND = SEC_INTERVAL * 60 * 3 // 3분
 
     val countDownTimer = object : CountDownTimer(MAX_SECOND, SEC_INTERVAL) {
         override fun onFinish() {
-            TODO("Not yet implemented")
+            authTimer.set("00:00")
         }
 
         override fun onTick(millisUntilFinished: Long) {
-            TODO("Not yet implemented")
+            authTimer.set(SimpleDateFormat("mm:ss").format(Date(millisUntilFinished)))
         }
-
     }
 
     init {
+        phoneNumber.set("")
         authTimer.set("")
-        confrimNumber.set("")
+        rtnNumber.set("")
     }
 
     // TODO : 휴대폰 번호 인증 요청
     fun requestPhoneNumberAuth() {
-
+        phoneNumber.get()?.let {
+            if (it.isEmpty()) return@let
+            countDownTimer.start()
+        }
     }
 
     // TODO : 휴대폰 인증 완료
-    fun confirmPhoneNumber() {
+    fun confirmRtnNumber() {
+        rtnNumber.get()?.let {
+            if (it.isEmpty()) return@let
 
+            navController.navigate(R.id.action_phoneAuthFragment_to_mainFragment)
+        }
     }
 }
