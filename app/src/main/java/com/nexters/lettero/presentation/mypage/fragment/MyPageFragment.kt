ab48f640aa521e826951311a4eb9309fda1621ef
songmilee <9676685+songmilee@users.noreply.github.com>
2021-02-14 13:25:56 +0900

    마이페이지 - 앱 셋팅 작성 (#7)
    * 마이페이지 레이아웃 작성
    
    * 마이페이지 레이아웃 추가
    
    * 마이 페이지 뷰모델과 뷰 연결
    
    * 휴대폰 번호 인증 화면 작성
    
    * 일반적인 UI 작업 처리
    
    * 사용자 전화번호 추출
    
    * 백버튼 처리 추가
    
    * livedata 변경

--- app/src/main/java/com/nexters/lettero/presentation/mypage/fragment/MyPageFragment.kt
+++ app/src/main/java/com/nexters/lettero/presentation/mypage/fragment/MyPageFragment.kt
@@ -1,5 +1,8 @@
 package com.nexters.lettero.presentation.mypage.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.nexters.lettero.R
 import com.nexters.lettero.databinding.FragmentMyPageBinding
 import com.nexters.lettero.presentation.base.BaseFragment
@@ -9,4 +12,15 @@ import com.nexters.lettero.presentation.mypage.viewmodel.MyPageViewModel
 class MyPageFragment : BaseFragment<FragmentMyPageBinding, MyPageViewModel>() {
     override val layoutRes: Int = R.layout.fragment_my_page
     override var viewModel: ViewModel = MyPageViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.view = this
+        binding.vm = viewModel as MyPageViewModel
+    }
+
+    fun moveToAppSetting(view: View) {
+        findNavController().navigate(R.id.action_myPageFragment_to_settingFragment)
+    }
 }
--- app/src/main/java/com/nexters/lettero/presentation/mypage/viewmodel/MyPageViewModel.kt
+++ app/src/main/java/com/nexters/lettero/presentation/mypage/viewmodel/MyPageViewModel.kt
@@ -2,4 +2,14 @@ package com.nexters.lettero.presentation.mypage.viewmodel
 
 import com.nexters.lettero.presentation.base.ViewModel
 
-class MyPageViewModel : ViewModel {}
+class MyPageViewModel : ViewModel {
+
+    var name: String
+    var phoneNumber: String
+
+    init {
+        //TODO : 사용자 정보 연동 후 변경 예정
+        name = "aaa"
+        phoneNumber = "010-0000-0000"
+    }
+}
