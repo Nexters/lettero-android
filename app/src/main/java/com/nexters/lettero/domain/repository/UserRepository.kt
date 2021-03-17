c7b8f01e1ed8ca03ae0759dc0edf9b29dc8ae721
songmilee <lsm941010@gmail.com>
2021-03-17 23:41:02 +0900

    UserRepository SharedPreference 지원을 위해 context 변수 추가

--- app/src/main/java/com/nexters/lettero/domain/repository/UserRepository.kt
+++ app/src/main/java/com/nexters/lettero/domain/repository/UserRepository.kt
@@ -1,5 +1,6 @@
 package com.nexters.lettero.domain.repository
 
+import com.google.gson.JsonObject
 import com.nexters.lettero.data.model.ThumbNailResponse
 import com.nexters.lettero.data.model.User
 import io.reactivex.Single
@@ -8,9 +9,13 @@ interface UserRepository {
 
     fun getUserInfo(): Single<User>
 
-    fun savePhoneNumber(number: String): Single<User>
+    fun savePhoneNumber(number: JsonObject): Single<User>
 
-    fun saveFirebaseToken(token: String): Single<User>
+    fun saveFirebaseToken(token: JsonObject): Single<User>
 
     fun getThumbNails(): Single<List<ThumbNailResponse>>
+
+    fun saveKeyValue(key: String, value: String)
+
+    fun getStringValue(key: String): String?
 }
