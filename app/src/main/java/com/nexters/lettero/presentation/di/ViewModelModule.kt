97e35498c77179dd6e49a4bba580dc80b9cb39e6
songmilee <lsm941010@gmail.com>
2021-03-17 23:43:20 +0900

    ViewModel Provide 추가

--- app/src/main/java/com/nexters/lettero/presentation/di/ViewModelModule.kt
+++ app/src/main/java/com/nexters/lettero/presentation/di/ViewModelModule.kt
@@ -2,9 +2,12 @@ package com.nexters.lettero.presentation.di
 
 import com.nexters.lettero.domain.interactor.KakaoLoginUseCase
 import com.nexters.lettero.domain.interactor.KakaoTokenUseCase
+import com.nexters.lettero.domain.repository.UserRepository
 import com.nexters.lettero.presentation.intro.viewmodel.IntroViewModel
 import com.nexters.lettero.presentation.login.viewmodel.LoginViewModel
+import com.nexters.lettero.presentation.login.viewmodel.PhoneAuthViewModel
 import com.nexters.lettero.presentation.main.viewmodel.MainViewModel
+import com.nexters.lettero.presentation.mypage.viewmodel.MyPageViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,10 +21,23 @@ object ViewModelModule {
     fun provideMainViewModel() = MainViewModel()
 
     @Provides
-    fun provideIntroViewModel(kakaoTokenUseCase: KakaoTokenUseCase) =
-        IntroViewModel(kakaoTokenUseCase)
+    fun provideIntroViewModel(
+        kakaoTokenUseCase: KakaoTokenUseCase,
+        userRepository: UserRepository
+    ) =
+        IntroViewModel(kakaoTokenUseCase, userRepository)
 
     @Provides
-    fun provideLoginViewModel(kakaoLoginUseCase: KakaoLoginUseCase) =
-        LoginViewModel(kakaoLoginUseCase)
+    fun provideLoginViewModel(
+        kakaoLoginUseCase: KakaoLoginUseCase,
+        userRepository: UserRepository
+    ) =
+        LoginViewModel(kakaoLoginUseCase, userRepository)
+
+    @Provides
+    fun providePhoneAuthViewModel(userRepository: UserRepository) =
+        PhoneAuthViewModel(userRepository)
+
+    @Provides
+    fun provideMyPageViewModel(userRepository: UserRepository) = MyPageViewModel(userRepository)
 }
